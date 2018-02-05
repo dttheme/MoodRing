8b91f841f452a762c11ac6342d616604325be68d
dttheme <dttheme@gmail.com>
2018-02-05 11:34:42 -0500

    Add remaining endpoints for router

--- postRouter.js
+++ postRouter.js
@@ -37,4 +37,33 @@ router.post('/posts', (req, res) => {
     })
 })
 
+router.put('/posts/:id', (req, res) => {
+  const toUpdate = {};
+  const updateableFields = ['mood', 'activity', 'note'];
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Post
+  .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+  .exec()
+  then(updatedPost => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+router.delete('/posts:id', (req, res) => {
+  Post
+    .findByIdAndRemove(req.params.id)
+    .exec()
+    .then(() => {
+      res.status(204).json({ message: 'Success' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
 module.exports = router;
