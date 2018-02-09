0dbf9b3c337c75622b11fc9d10d5dff8da9ad111
dttheme <dttheme@gmail.com>
2018-02-09 10:40:27 -0500

    Build frontend framwork, HTML, CSS, JS

--- public/posts.js
+++ public/posts.js
@@ -45,21 +45,49 @@ function getPreviousEntries(callbackFn) {
 }
 
 
+
 //object literal looks better!
 function displayPreviousEntries(data) {
-  for (index in data.posts) {
-    $('.post-group').append(
-      `<div class='post'>
-      <p>${data.posts[index].publishedAt}</p>
-        <p>On this day:</p>
-          <p>I felt: ${data.posts[index].mood} </p>
-          <p>I accomplished: ${data.posts[index].activity} </p>
-          <p> What else?: ${data.posts[index].note}</p>
-        </div>
-      `
-    );
-    console.log(data.posts[index]);
+  if (data.length === 0) {
+    $('post_group').html(`<h2 class='no results'> You don't have any posts yet! Click 'Add New Post' to get started.`)
+  } else {
+    for (index in data.posts) {
+      $('.post_group').append(
+        `<div class='post' id=${data.posts[index].id}>
+            <p>${data.posts[index].publishedAt}</p>
+            <p>On this day:</p>
+            <p>I felt: ${data.posts[index].mood} </p>
+            <p>I accomplished: ${data.posts[index].activity} </p>
+            <p> What else?: ${data.posts[index].note}</p>
+            <button type='button' class='delete_post_button'>Delete</button>
+            <button type='button' class='edit_post_button'>Edit</button>
+          </div>
+        `
+      );
+      // console.log(data.posts[index]);
+      deletePreviousEntries();
+    }
   }
+}
+
+function deletePreviousEntries() {
+  $('.delete_post_button').on('click', function(event) {
+    const postId = $(this).closest('div').attr('id');
+    $.ajax({
+      url:`/posts/${postId}`,
+      type: 'DELETE',
+      dataType: 'json',
+
+      success: function(data){console.log(`Successfully deleted post ${postId}`)},
+      error: function(jqXHR, exception) {
+        console.log(jqXHR);
+        console.log(exception);}
+    });
+
+    setTimeout(function() {
+      location.reload(true);
+    }, 700);
+  });
 }
 
 function getAndDisplayPreviousEntries() {
