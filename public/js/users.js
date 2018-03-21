65a8b9f756b58f7fcc4cab5113db4c192b1c5819
dttheme <dttheme@gmail.com>
2018-03-20 20:30:40 -0400

    Adjust login page media queries, reconfigure mLab DB

--- public/js/users.js
+++ public/js/users.js
@@ -39,7 +39,6 @@ function postNewUser(firstName, username, password) {
         }
       },
       error: (...rest) => {
-        console.log(rest);
         $('#sign_up_form').prepend(
           `
           <div class='sign_up_failure'><span>Whoops! This email has already been used for signup. </span></div>
